1e65194ead15b85076b8d748ec6db73c3d70d11f
Panthadip Maji <majipanthadip@gmail.com>
2026-02-25 02:00:09 +0530

    port fix

--- server.js
+++ server.js
@@ -84,4 +84,5 @@ upload.fields([
     res.json({ status:"success", id: applicantID });
 });
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
